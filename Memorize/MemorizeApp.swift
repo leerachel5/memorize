03229b10b5542a9d9a1e0e2b7c4afd66ab9acc58
Rachel Lee <leerachel481@gmail.com>
2024-05-13 15:33:06 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Rachel Lee on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
